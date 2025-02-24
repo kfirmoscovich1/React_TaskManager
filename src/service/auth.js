bf1ab024a718fe2f4217a9fa2ec9c4f835b0950f
Kfir Moscovich <kfirmoscovich1@gmail.com>
2025-02-24 09:24:02 +0200

    Add files via upload

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { auth, firestore } from "./firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
@@ -63,69 +62,3 @@ onAuthStateChanged(auth, (user) => {
 });
 
 export { auth };
-=======
-import { auth, firestore } from "./firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
-
-export const registerWithEmail = async (email, password, fullName) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    await setDoc(doc(firestore, "users", user.uid), {
-      fullName,
-      email,
-    });
-
-    console.log("User registered successfully:", user.uid);
-    return user;
-  } catch (error) {
-    console.error("Error registering user:", error);
-    throw error;
-  }
-};
-
-export const loginWithEmail = async (email, password) => {
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    console.log("User logged in successfully:", user.uid);
-    return user;
-  } catch (error) {
-    console.error("Error logging in user:", error);
-    throw error;
-  }
-};
-
-export const loginWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  return signInWithPopup(auth, provider);
-};
-
-export const logout = () => signOut(auth);
-
-export const getCurrentUserId = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      unsubscribe();
-      if (user) {
-        resolve(user.uid);
-      } else {
-        reject(new Error("User not authenticated"));
-      }
-    });
-  });
-};
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("User is authenticated.");
-  } else {
-    console.log("No user is authenticated.");
-  }
-});
-
-export { auth };
->>>>>>> b892819 (Initial commit)
